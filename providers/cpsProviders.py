c0a3f6b649bcce0e006266dc9cad0f900007d65d
wieta <wieta2143@gmail.com>
2016-08-03 15:02:21 +0200

    Modify cpsProvider

--- providers/cpsProviders.py
+++ providers/cpsProviders.py
@@ -1,9 +1,11 @@
 from faker import Factory
 from providers.customerProvider import CustomerProvider
+from providers.clientRegisterProvider import ClientRegisterProvider
 
 class Providers():
 
 	def all():
 		f = Factory.create('pl_PL')
 		f.add_provider(CustomerProvider)
+		f.add_provider(ClientRegisterProvider)
 		return f
