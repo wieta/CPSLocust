4166fc268af19cb1aea11f1c79d65fdd5718e30b
mpanowicz <panowicz.michal@gmail.com>
2016-08-04 10:01:56 +0200

    Add update codes

--- response/webSticker/responses.py
+++ response/webSticker/responses.py
@@ -1,3 +1,4 @@
 ﻿from response.webSticker.login import Login as login
 from response.webSticker.logout import Logout as logout
 from response.webSticker.get import Get as get
+from response.webSticker.update import Update as update
