2a56801ed70983657e678752b0bd8eb2e7c5aa89
mpanowicz <panowicz.michal@gmail.com>
2016-08-04 10:00:39 +0200

    Add codes

--- response/webSticker/get.py
+++ response/webSticker/get.py
@@ -7,3 +7,27 @@ class Get():
 			'Message' : 'Dane klienta nie zostały zmodyfikowane'
 		}
 		return r
+
+	def noAuthorization():
+		r = {
+			'HttpCode' : '403',
+			'MessageCode': 'NoAuthorization',
+			'Message' : 'Brak uprawnień'
+		}
+		return r
+
+	def sessionExpired():
+		r = {
+			'HttpCode' : '403',
+			'MessageCode': 'SessionExpired',
+			'Message' : ''
+		}
+		return r
+
+	def customerNotFoundByCardAndPhoneNumber():
+		r = {
+			'HttpCode' : '404',
+			'MessageCode': 'CustomerNotFoundByCardAndPhoneNumber',
+			'Message' : 'Klient o zadanym numerze karty i telefonu nie istnieje'
+		}
+		return r
